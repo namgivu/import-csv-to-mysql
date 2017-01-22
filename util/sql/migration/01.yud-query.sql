361868f580d0a9edaed8a8b9755bf1ca5b9f4ac6
namgivu <namgivu@gmail.com>
2017-01-22 16:53:59 +0700

    query TODO

---
+++ util/sql/migration/01.yud-query.sql
@@ -0,0 +1,11 @@
+#region Customers who buy again and again e.g. 3 times, 5 times, etc.
+#endregion Customers who buy again and again e.g. 3 times, 5 times, etc.
+
+#region Customers who buy the most time
+#endregion Customers who buy the most time
+
+#region At what buying time, e.g. 2 times, will a customer always return?
+#endregion At what buying time, e.g. 2 times, will a customer always return?
+
+#region What names of famous brands who buy from us?
+#endregion What names of famous brands who buy from us?
